783805610cfe37f8fc22662d6bf35361c607ed2e
FrancoMarchetta <frncmarchetta@gmail.com>
2025-02-22 17:46:56 -0300

    better copy button and icon

--- src/pages/PasswordGenerator.tsx
+++ src/pages/PasswordGenerator.tsx
@@ -42,10 +42,16 @@ function PasswordGenerator() {
        
       <div className='input-and-copy-btn'>
         <input ref={inputRef} value={newPassword} type="text" placeholder='Generated Password' name="" id="" />
-        <button onClick={copyToClipboard}><img id='copyIcon' src={copyIcon} alt="copy" /> </button>
+        <button onClick={copyToClipboard} className="Btn">
+        <svg viewBox="0 0 512 512" className="svgIcon" height="1em"><path d="M288 448H64V224h64V160H64c-35.3 0-64 28.7-64 64V448c0 35.3 28.7 64 64 64H288c35.3 0 64-28.7 64-64V384H288v64zm-64-96H448c35.3 0 64-28.7 64-64V64c0-35.3-28.7-64-64-64H224c-35.3 0-64 28.7-64 64V288c0 35.3 28.7 64 64 64z"></path></svg>
+        {/* <p className="text">COPY</p> */}
+        <span className="effect"></span>
+      </button>
       </div>
 
       <button onClick={createPassword}>Generate new Password</button>
+            
+     
 
       <div>
         <button id='github-btn'>gitHub</button>
