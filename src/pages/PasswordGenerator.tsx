a169b77f25e7509550148951c972460693071315
FrancoMarchetta <frncmarchetta@gmail.com>
2025-02-22 16:00:57 -0300

    fixes

--- src/pages/PasswordGenerator.tsx
+++ src/pages/PasswordGenerator.tsx
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from 'react'
+import  { useRef, useState } from 'react'
 import "./css/PasswordGenerator.css"
 import copyIcon from "../assets/copy.png"
 function PasswordGenerator() {
