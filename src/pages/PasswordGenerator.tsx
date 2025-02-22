dc167fa7fd19c5b35bf671ad5df8d069337f200f
FrancoMarchetta <frncmarchetta@gmail.com>
2025-02-22 14:31:06 -0300

    it generates the password

--- src/pages/PasswordGenerator.tsx
+++ src/pages/PasswordGenerator.tsx
@@ -1,8 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./css/PasswordGenerator.css"
 
- function PasswordGenerator() {
-    return (
+function PasswordGenerator() {
+
+  const lowercaseLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+  const uppercaseLetters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+  const numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
+  const symbols = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "=", "+"];
+
+
+  const allCharacters = lowercaseLetters.concat(uppercaseLetters, numbers, symbols); //concatena los 3 arrays en uno solo
+
+  // console.log(allCharacters);
+  
+  const [newPassword, setNewPasword] = useState("");
+
+  function createPassword(){
+    let password = "";
+    for(let i = 0; i<=10; i++){
+      const randomIndex = Math.floor(Math.random() * allCharacters.length)
+      password += allCharacters[randomIndex];
+    }
+    setNewPasword(password);
+    console.log(newPassword);
+  }
+
+  return (
     <>
     <main className='container'>
       <div className='pswrd-container'>
@@ -11,8 +34,8 @@ import "./css/PasswordGenerator.css"
         <h1 className='cursor typewriter-animation' color='rgb(4, 255, 0)'> Password Generator</h1>
       </div>  
       
-      <input type="text" placeholder='Generated Password' name="" id="" />
-      <button>Generate new Password</button>
+      <input value={newPassword} type="text" placeholder='Generated Password' name="" id="" />
+      <button onClick={createPassword}>Generate new Password</button>
 
       <div>
         <button id='github-btn'>gitHub</button>
